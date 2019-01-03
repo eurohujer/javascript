9372e4af462798afd6a6300bec00f9a2c7b297e4
eurohujer <45887027+eurohujer@users.noreply.github.com>
2019-01-03 16:00:54 +0100

    Create cistenie.js

---
+++ cistenie.js
@@ -0,0 +1,13 @@
+// modul cistenie.js
+
+// zaciatok definicie cistenie tagov
+function cistenie() {
+$("#dropletid").text("");
+$("#postodpoved1").text("");
+$("#postodpoved2").text("");
+$("#postodpoved3").text("");
+$("#postodpoved4").text("");
+$("#postodpoved4").attr("href", "");
+}// koniec definicie cistenie tagov
+
+export { cistenie };
